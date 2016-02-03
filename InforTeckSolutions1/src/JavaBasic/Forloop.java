0eff57952b5f062a82eb934795df71ed618c4325
ayana_000 <ayana_000@sol>
2016-02-02 22:18:16 -0500

    First Repository

---
+++ InforTeckSolutions1/src/JavaBasic/Forloop.java
@@ -0,0 +1,31 @@
+package JavaBasic;
+
+public class  Forloop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int age = 25;
+		System.out.print("solomon's age is ="  + age + "dfdfsdfsdfsdf" + "\n");
+		String myreturneddata= myAgeFuncion(age);
+		for(int i =1; i <=10; i++){
+			System.out.println(i + "."  +  myreturneddata);
+		}
+		
+	
+	}
+	
+	
+	
+	private static  String  myAgeFuncion(int myage){
+		String  myreturn;
+		if(myage > 0 && myage < 49 ) {
+		myreturn= "yourng"; 
+	}else if(myage > 49 &&   myage<100){
+		myreturn= "old";
+	}else{
+		myreturn= "out of range";
+	}
+		return myreturn;
+}
+
+}
